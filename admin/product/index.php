445593b818626e91a33c5542337e6421442b42f8
Shafik <shaifk108@gmail.com>
2024-05-18 16:52:06 +0600

    Edit Product

--- admin/product/index.php
+++ admin/product/index.php
@@ -6,6 +6,8 @@ if (isset($_GET['msg']) && $_GET['msg'] == 'add') {
     include "./addProduct.php";
 } else if (isset($_GET['msg']) && $_GET['msg'] == 'edit') {
     include "./editProduct.php";
+    editProduct($_GET['id']);
+    
 } else if (isset($_GET['msg']) && $_GET['msg'] == 'delete') {
     include "./deleteProduct.php";
 }  else if (isset($_GET['msg']) && $_GET['msg'] == 'view') {
